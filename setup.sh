d2cc8c3865859585f2005d3266c3f562fc145be9
leegbestand <leegbestand@gmail.com>
2019-08-01 15:40:42 +0200

    Update setup with possible different namings.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-COCONUT_DATA_DIR=coconut-data
+COCONUT_DIR=$1
+COCONUT_DATA_DIR=$2
 
-make -C CoCoNut &&
+make -C $(COCONUT_DIR) &&
 mkdir $COCONUT_DATA_DIR &&
 mkdir $COCONUT_DATA_DIR/src/ &&
 mkdir $COCONUT_DATA_DIR/include/ &&
 mkdir $COCONUT_DATA_DIR/src/ccnstd/ &&
-cp -r CoCoNut/src/core $COCONUT_DATA_DIR/src/ &&
-cp -r CoCoNut/include/core $COCONUT_DATA_DIR/include/ &&
-cp CoCoNut/coconut.mk $COCONUT_DATA_DIR/Makefile &&
-cp CoCoNut/CoCoNut-lib/source/* $COCONUT_DATA_DIR/src/ccnstd/ &&
-cp -r CoCoNut/CoCoNut-lib/include/lib $COCONUT_DATA_DIR/include
+cp -r $(COCONUT_DIR)/src/core $COCONUT_DATA_DIR/src/ &&
+cp -r $(COCONUT_DIR)/include/core $COCONUT_DATA_DIR/include/ &&
+cp $(COCONUT_DIR)/coconut.mk $COCONUT_DATA_DIR/Makefile &&
+cp $(COCONUT_DIR)/CoCoNut-lib/source/* $COCONUT_DATA_DIR/src/ccnstd/ &&
+cp -r $(COCONUT_DIR)/CoCoNut-lib/include/lib $COCONUT_DATA_DIR/include
