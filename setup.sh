b6d2d5a3ec17b1591e923e28224c2673f6f2974e
leegbestand <leegbestand@gmail.com>
2019-08-23 20:27:28 +0200

    Setup process requires coconut_flags.

--- setup.sh
+++ setup.sh
@@ -18,4 +18,5 @@ cp -r $COCONUT_DIR/src/core $COCONUT_DATA_DIR/src/ &&
 cp -r $COCONUT_DIR/include/core $COCONUT_DATA_DIR/include/ &&
 cp $COCONUT_DIR/coconut.mk $COCONUT_DATA_DIR/Makefile &&
 cp $COCONUT_DIR/CoCoNut-lib/source/* $COCONUT_DATA_DIR/src/ccnstd/ &&
-cp -r $COCONUT_DIR/CoCoNut-lib/include/lib $COCONUT_DATA_DIR/include
+cp -r $COCONUT_DIR/CoCoNut-lib/include/lib $COCONUT_DATA_DIR/include &&
+cp $COCONUT_DIR/coconut_flags.mk $COCONUT_DATA_DIR/
