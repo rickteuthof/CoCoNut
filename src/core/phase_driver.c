404a4ad952d6ad87bcb661ae4122b34dd659db3d
leegbestand <leegbestand@gmail.com>
2019-10-26 18:00:50 +0200

    Remove the set n for time printing.

--- src/core/phase_driver.c
+++ src/core/phase_driver.c
@@ -10,13 +10,10 @@ void ccn_phase_driver_destroy() {
 }
 
 
-void ccn_set_time_items_printed(size_t n) {
-    return ccn_set_print_n(n);
-}
-
 bool ccn_set_breakpoint(char *breakpoint) {
     _ccn_set_breakpoint(breakpoint);
 }
+
 bool ccn_set_inspectpoint(char *inspect) {
     _ccn_set_inspect_point(inspect);
 }
